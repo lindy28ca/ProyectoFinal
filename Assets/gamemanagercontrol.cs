9f1beb5dd1bd92cbb8d6bf887cf406457fe83e47
lindy28ca <aliagagreis@gmail.com>
2024-06-20 00:42:35 -0500

    jueguito

---
+++ Assets/gamemanagercontrol.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gamemanagercontrol : MonoBehaviour
+{
+    public Text puntos;
+
+}
--- Assets/movimientos.cs
+++ Assets/movimientos.cs
@@ -7,6 +7,7 @@ public class movimientos : MonoBehaviour
 {
     Rigidbody2D helado;
     public float velocidad;
+    public cambiarescena cambiar;
 
     private void Awake()
     {
@@ -15,22 +16,29 @@ public class movimientos : MonoBehaviour
     private void FixedUpdate()
     {
         helado.velocity = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             helado.velocity = new Vector2(velocidad,0);
         }
-        else if (Input.GetKey(KeyCode.W))
+        else if (Input.GetKey(KeyCode.UpArrow))
         {
             helado.velocity = new Vector2(0, velocidad);
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.DownArrow))
         {
             helado.velocity = new Vector2(0, -velocidad);
         }
-        else if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
             helado.velocity = new Vector2( -velocidad,0 );
         }
      
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "enemigo")
+        {
+            cambiar.escenas("perdiste");
+        }
     }
 }
